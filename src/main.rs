b38bcdeaacaba23bc2c93747212ca047d2216889
Ben Püschel <ben.pueschel2004@gmail.com>
2024-06-10 19:44:34 +0200

    better screen information

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,28 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use colored::Colorize;
 use math::Vector;
 use network::{Network, TrainingData};
 
+use crate::screen::ScreenInfo;
+
 pub mod math;
 pub mod network;
+pub mod screen;
 pub mod serializer;
 
 pub mod mnist;
 
 static PEEK_COUNT: usize = 5;
-static BATCHES: usize = 60;
+static BATCHES: usize = 20;
 static THREAD_COUNT: usize = 10;
-static LEARNING_RATE: f64 = 0.1;
+static LEARNING_RATE: f64 = 0.01;
 static NETWORK_PATH: &str = "network.ben";
 
 fn main() {
     let exit = Arc::new(Mutex::new(false));
     let exit_clone = exit.clone();
+
     ctrlc::set_handler(move || {
         let mut exit = exit_clone.lock().unwrap();
         *exit = true;
@@ -43,37 +46,50 @@ fn main() {
     let mut test_data = Vec::from(&test_set);
     rand::seq::SliceRandom::shuffle(test_data.as_mut_slice(), &mut rand::thread_rng());
 
-    for i in 0.. {
-        let start = std::time::Instant::now();
-        let stats = test_network(&network, &test_data, true);
+    let start = std::time::Instant::now();
+    let mut screen_info = ScreenInfo::default();
+    screen::clear_screen();
 
-        println!("Training network...");
+    for i in 0.. {
+        screen_info.epoch = i;
+        screen_info.total_batches = BATCHES;
+        screen_info.batch = 0;
+        screen_info.status = "Testing...";
+        screen::display_info(&screen_info);
+
+        let stats = test_network(&network, &test_data, true);
+        screen_info.test_accuracy = stats.accuracy;
+        screen_info.test_loss = stats.avg_cost;
+        screen_info.test_confidence = stats.confidence;
+        screen_info.error_least_confident = stats.error_least_confident;
+        screen_info.error_least_confident_data = stats.error_least_confident_data;
+        screen::display_info(&screen_info);
+        screen_info.status = "Training...";
 
         for i in 0..BATCHES {
+            screen_info.batch = i + 1;
+            screen_info.elapsed = start.elapsed();
+
             let batch_size = training_data.len() / BATCHES;
             let start = i * batch_size;
             let end = start + batch_size;
 
             network.train_parallel(&training_data[start..end], LEARNING_RATE, THREAD_COUNT);
             // network.train(&training_data[start..end], LEARNING_RATE);
-        }
 
-        println!();
-        println!("Iteration {} results:", i);
-        println!("  Average cost: {}", stats.avg_cost);
-        println!("  Accuracy: {}", stats.accuracy);
-        println!("  Confidence: {}", stats.confidence);
-        println!("  Time: {:1?}", start.elapsed());
-        println!();
+            screen::display_info(&screen_info);
 
-        if *exit.lock().unwrap() {
-            serializer::serialize(&network, NETWORK_PATH).unwrap();
+            if *exit.lock().unwrap() {
+                // screen::move_cursor();
 
-            assert_eq!(serializer::deserialize(NETWORK_PATH).unwrap(), network);
+                serializer::serialize(&network, NETWORK_PATH).unwrap();
 
-            println!("Network saved to network.ben");
-            println!("Exiting...");
-            return;
+                assert_eq!(serializer::deserialize(NETWORK_PATH).unwrap(), network);
+
+                println!("Network saved to network.ben");
+                println!("Exiting...");
+                return;
+            }
         }
     }
 }
@@ -118,19 +134,21 @@ pub struct TestResult {
     pub accuracy: f64,
     pub confidence: f64,
     pub error_least_confident: Option<(Vector, f64)>,
+    pub error_least_confident_data: Option<TrainingData>,
 }
+
 pub fn test_network(network: &Network, dataset: &Vec<TrainingData>, peek: bool) -> TestResult {
     let mut avg_cost = 0.0;
     let mut accuracy = 0.0;
     let mut confidence = 0.0;
     let mut error_least_confident = None;
+    let mut error_least_confident_data = None;
 
     let mut peek_count = 0;
     for data in dataset.iter() {
-
         let output = network.feed_forward(data.input.clone());
         let predicted = output.argmax();
-        let current_cost = network.cost(&output, &data.target) / dataset.len() as f64;
+        let current_cost = network.cost(&output, &data.target);
         let current_confidence = math::softmax(output.clone())[predicted];
 
         if predicted == data.target.argmax() {
@@ -138,13 +156,16 @@ pub fn test_network(network: &Network, dataset: &Vec<TrainingData>, peek: bool) 
         } else if peek && peek_count < PEEK_COUNT {
             match error_least_confident {
                 Some((_, confidence)) if confidence < current_confidence => (),
-                _ => error_least_confident = Some((output.clone(), current_confidence)),
+                _ => {
+                    error_least_confident = Some((output.clone(), current_confidence));
+                    error_least_confident_data = Some(data.clone());
+                }
             }
             peek_count += 1;
         }
 
-        avg_cost += current_cost;
-        confidence += current_confidence;
+        avg_cost += current_cost / dataset.len() as f64;
+        confidence += current_confidence / dataset.len() as f64;
     }
 
     TestResult {
@@ -152,29 +173,6 @@ pub fn test_network(network: &Network, dataset: &Vec<TrainingData>, peek: bool) 
         accuracy,
         confidence,
         error_least_confident,
+        error_least_confident_data,
     }
-}
-
-fn print_image(data: &Vector, size: (usize, usize)) {
-    for i in 0..size.0 {
-        for j in 0..size.1 {
-            let value = data.at(i * size.1 + j);
-            let value = (value * 255.0) as u8;
-            // use ▉ as a character to represent the pixel, use colored crate for grayscale output
-            let pixel = "▉".truecolor(value, value, value);
-            print!("{}", pixel);
-        }
-        println!();
-    }
-}
-
-fn peek_error(output: &Vector, data: &TrainingData) {
-    let predicted = output.argmax();
-    let actual = data.target.argmax();
-    let confidence = math::softmax(output.clone())[predicted];
-
-    print_image(&data.input, (28, 28));
-    println!("digit: {}, prediction: {}", actual, predicted);
-    println!("confidence: {:.5?}", confidence);
-    println!("output: {:.5?}", output.0);
 }
---
+++ src/screen.rs
@@ -0,0 +1,96 @@
+use std::time::Duration;
+
+use colored::Colorize;
+
+use crate::{
+    math::{self, Vector},
+    network::TrainingData,
+};
+
+pub fn clear_screen() {
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+}
+
+pub fn move_cursor() {
+    print!("{esc}[1;1H", esc = 27 as char);
+}
+
+#[derive(Default)]
+pub struct ScreenInfo {
+    pub epoch: usize,
+    pub batch: usize,
+    pub total_batches: usize,
+    pub elapsed: Duration,
+    pub test_accuracy: f64,
+    pub test_loss: f64,
+    pub test_confidence: f64,
+    pub error_least_confident: Option<(Vector, f64)>,
+    pub error_least_confident_data: Option<TrainingData>,
+    pub status: &'static str,
+    pub exit: bool,
+}
+
+pub fn display_info(info: &ScreenInfo) {
+    //clear_screen();
+    move_cursor();
+
+    match &info.error_least_confident {
+        Some(x) => {
+            let output = &x.0;
+            let data = info.error_least_confident_data.as_ref().unwrap();
+
+            let predicted = output.argmax();
+            let actual = data.target.argmax();
+            let confidence = math::softmax(output.clone())[predicted];
+
+            print_image(&data.input, (28, 28));
+            println!(
+                "digit: {}, prediction: {}",
+                actual, predicted
+            );
+            println!("confidence: {:.5?}", confidence);
+            println!("output: {:.5?}", output.0);
+        }
+        None => {
+            let _ = (0..32).map(|_| println!());
+        }
+    };
+    println!();
+    println!(
+        "Epoch {:#03} (batch {:#03}/{:#03}):",
+        info.epoch, info.batch, info.total_batches
+    );
+    println!(" Average loss: {:.10}", info.test_loss);
+    println!(" Accuracy:     {:.10}", info.test_accuracy);
+    println!(" Confidence:   {:.5}", info.test_confidence);
+    println!(" Time elapsed: {:.1}s", info.elapsed.as_secs_f32());
+    if info.exit {
+        println!(" Waiting to exit...");
+    }
+    println!();
+    println!("Status: {}", info.status);
+}
+
+fn print_image(data: &Vector, size: (usize, usize)) {
+    for i in 0..size.0 {
+        for j in 0..size.1 {
+            let value = data.at(i * size.1 + j);
+            let value = (value * 255.0) as u8;
+            // use ▉ as a character to represent the pixel, use colored crate for grayscale output
+            let pixel = "▉".truecolor(value, value, value);
+            print!("{}{}", pixel, pixel);
+        }
+        println!();
+    }
+}
+
+fn peek_error(output: &Vector, data: &TrainingData) {
+    let predicted = output.argmax();
+    let actual = data.target.argmax();
+    let confidence = math::softmax(output.clone())[predicted];
+
+    print_image(&data.input, (28, 28));
+    println!("digit: {}, prediction: {}", actual, predicted);
+    println!("confidence: {:.5?}", confidence);
+    println!("output: {:.5?}", output.0);
+}
